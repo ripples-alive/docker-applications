efeeaf9da492aea4a37a2b85b2a194695f016abf
ripples <564311873@qq.com>
2018-04-03 21:48:50 +0800

    feat(ctf-box): optimize debug related config

--- ctf/ctf-box/extra.zsh
+++ ctf/ctf-box/extra.zsh
@@ -1,1 +1,8 @@
+#!/bin/zsh
+
 source /usr/local/bin/virtualenvwrapper.sh
+
+alias dbg='export LD_LIBRARY_PATH=/dbg/lib; echo use ld path: $LD_LIBRARY_PATH'
+alias dbg32='export LD_LIBRARY_PATH=/dbg/lib32; echo use ld path: $LD_LIBRARY_PATH'
+
+export PYTHONIOENCODING=utf-8
