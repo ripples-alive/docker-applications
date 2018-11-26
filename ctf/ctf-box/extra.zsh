d7693e91e9ba28ba35716bb31e295dd9b0c75df7
ripples <564311873@qq.com>
2018-11-26 14:35:37 +0800

    fix(ctf-box): fix alias for quickly enabling libc debug

--- ctf/ctf-box/extra.zsh
+++ ctf/ctf-box/extra.zsh
@@ -13,7 +13,7 @@ EOF
 
 source /usr/local/bin/virtualenvwrapper.sh
 
-alias dbg='export LD_LIBRARY_PATH=/dbg/lib; echo use ld path: $LD_LIBRARY_PATH'
-alias dbg32='export LD_LIBRARY_PATH=/dbg/lib32; echo use ld path: $LD_LIBRARY_PATH'
+alias dbg64='export LD_LIBRARY_PATH=/dbg64/lib; echo use ld path: $LD_LIBRARY_PATH'
+alias dbg32='export LD_LIBRARY_PATH=/dbg32/lib; echo use ld path: $LD_LIBRARY_PATH'
 
 export PYTHONIOENCODING=utf-8
