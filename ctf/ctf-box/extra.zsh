9c6275f747ed96d72fbb1c2e002ee600d4999445
ripples <564311873@qq.com>
2018-12-20 11:38:33 +0800

    feat(ctf-box): update zsh plugin list & lazyload virtualenv

--- ctf/ctf-box/extra.zsh
+++ ctf/ctf-box/extra.zsh
@@ -1,17 +1,12 @@
 #!/bin/zsh
 
 antigen bundles <<EOF
-    adb
-    brew
-    copydir
-    copyfile
-    cp
-    jsontools
-    pip
-    urltools
 EOF
 
-source /usr/local/bin/virtualenvwrapper.sh
+function _load_workon {
+    source /usr/local/bin/virtualenvwrapper.sh
+}
+lazyload workon
 
 alias dbg64='export LD_LIBRARY_PATH=/dbg64/lib; echo use ld path: $LD_LIBRARY_PATH'
 alias dbg32='export LD_LIBRARY_PATH=/dbg32/lib; echo use ld path: $LD_LIBRARY_PATH'
